86dc701821fa1488f5b3ea8670b00321bc3382a6
Radoslaw Fedor <radoslaw.fedor@gmail.com>
2021-03-05 10:42:28 +0100

    create facility jpa repository

--- src/main/java/com/github/fredO1211/booking/repository/FacilityRepository.java
+++ src/main/java/com/github/fredO1211/booking/repository/FacilityRepository.java
@@ -5,12 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface FacilityRepository extends JpaRepository<Facility, Integer> {
-    Optional<Facility> findById(Integer id);
-
     @Query(nativeQuery = true, value = "SELECT count(*)>0 FROM FACILITIES WHERE NAME=:name")
-    boolean isFacilityNameExists(String name);
+    boolean isFacilityExists(String name);
 }
