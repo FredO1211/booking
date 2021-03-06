007f2bf033f5504e07299f533f8f1b52ccd60719
Radoslaw Fedor <radoslaw.fedor@gmail.com>
2021-03-06 09:43:11 +0100

    add IncorrectInputDataException class

---
+++ src/main/java/com/github/fredO1211/booking/service/exceptions/IncorrectInputDataException.java
@@ -0,0 +1,7 @@
+package com.github.fredO1211.booking.service.exceptions;
+
+public class IncorrectInputDataException extends RuntimeException{
+    IncorrectInputDataException(Throwable e) {
+        super(e);
+    }
+}
