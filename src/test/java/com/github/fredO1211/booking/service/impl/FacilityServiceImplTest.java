dd3c73cd25d4584272952e9fe030a9ae142d7d2f
Radoslaw Fedor <radoslaw.fedor@gmail.com>
2021-03-08 19:42:36 +0100

    create tests for facility service

--- src/test/java/com/github/fredO1211/booking/service/impl/FacilityServiceImplTest.java
+++ src/test/java/com/github/fredO1211/booking/service/impl/FacilityServiceImplTest.java
@@ -2,11 +2,15 @@ package com.github.fredO1211.booking.service.impl;
 
 import com.github.fredO1211.booking.domain.Facility;
 import com.github.fredO1211.booking.repository.FacilityRepository;
+import com.github.fredO1211.booking.service.exceptions.ElementDoesNotExistException;
 import com.github.fredO1211.booking.service.exceptions.UnavailableNameException;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -23,10 +27,19 @@ class FacilityServiceImplTest {
 
     @Test
     void shouldThrowUnavailableNameExceptionWhenNameIsExist() {
-        //given
+        // given
         Facility facility = new Facility("name", 30);
         when(repository.isFacilityExists(anyString())).thenReturn(true);
         // when + then
         assertThrows(UnavailableNameException.class, () -> service.valid(facility));
+    }
+
+    @Test
+    void shouldThrowElementDoesNotExistExceptionWhenRepositoryReturnNull(){
+        // given
+        when(repository.findById(anyLong())).thenReturn(Optional.empty());
+        // when + then
+        assertThrows(ElementDoesNotExistException.class,
+                () -> service.update(1L, new Facility("name",211)));
     }
 }
