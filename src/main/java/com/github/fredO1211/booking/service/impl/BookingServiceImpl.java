7fd56731b95e1fa308f33cd4f70c3d65b89d78a7
Radoslaw Fedor <radoslaw.fedor@gmail.com>
2021-03-08 19:29:24 +0100

    add EmailProvider

--- src/main/java/com/github/fredO1211/booking/service/impl/BookingServiceImpl.java
+++ src/main/java/com/github/fredO1211/booking/service/impl/BookingServiceImpl.java
@@ -3,6 +3,7 @@ package com.github.fredO1211.booking.service.impl;
 import com.github.fredO1211.booking.domain.Booking;
 import com.github.fredO1211.booking.repository.BookingRepository;
 import com.github.fredO1211.booking.service.BookingService;
+import com.github.fredO1211.booking.service.MailProvider;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
@@ -12,9 +13,11 @@ import java.util.Optional;
 @Service
 public class BookingServiceImpl implements BookingService {
     public final BookingRepository repository;
+    public final MailProvider provider;
 
-    public BookingServiceImpl(BookingRepository repository) {
+    public BookingServiceImpl(BookingRepository repository, EmailProvider provider) {
         this.repository = repository;
+        this.provider = provider;
     }
 
     @Override
