5d44128abd2708fb42ba457d2cd19abb606d846f
Radoslaw Fedor <radoslaw.fedor@gmail.com>
2021-03-09 14:29:38 +0100

    add GuestDTO constructor

--- src/main/java/com/github/fredO1211/booking/service/dto/GuestDTO.java
+++ src/main/java/com/github/fredO1211/booking/service/dto/GuestDTO.java
@@ -1,9 +1,17 @@
 package com.github.fredO1211.booking.service.dto;
 
+import com.github.fredO1211.booking.domain.Guest;
+
 public class GuestDTO {
     private String name;
     private String email;
     private String phoneNumber;
+
+    GuestDTO(Guest guest) {
+        this.name = guest.getName();
+        this.email = guest.getEmail();
+        this.phoneNumber = guest.getPhoneNumber();
+    }
 
     public String getName() {
         return name;
