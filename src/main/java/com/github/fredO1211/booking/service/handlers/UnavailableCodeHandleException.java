5fccd053f706d55e1a54195574fffcb4a6171570
Radoslaw Fedor <radoslaw.fedor@gmail.com>
2021-03-11 14:10:06 +0100

    refactor

--- src/main/java/com/github/fredO1211/booking/service/handlers/UnavailableCodeHandleException.java
+++ src/main/java/com/github/fredO1211/booking/service/handlers/UnavailableCodeHandleException.java
@@ -6,10 +6,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class UnavailableCodeHandleException extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(UnavailableCodeException.class)
