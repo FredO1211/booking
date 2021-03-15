a9a423b6d66a92d5c2c0b0da588d930324fdc70d
Radoslaw Fedor <radoslaw.fedor@gmail.com>
2021-03-15 12:51:52 +0100

    add api info

--- src/main/java/com/github/fredO1211/booking/config/Config.java
+++ src/main/java/com/github/fredO1211/booking/config/Config.java
@@ -6,8 +6,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
 
 @Configuration
 public class Config {
@@ -20,8 +24,21 @@ public class Config {
     public Docket apiDocket() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.github.fredO1211.booking.controller"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(createApiInfo());
+    }
+
+    private ApiInfo createApiInfo() {
+        return new ApiInfo("Booking API",
+                "Booking API Documentation",
+                "1.0",
+                "https://github.com/FredO1211",
+                new Contact("Radosław Fedor","https://github.com/FredO1211", "radoslaw.fedor@gmail.com"),
+                "Apache 2.0",
+                "http://www.apache.org/licenses/LICENSE-2.0",
+                Collections.emptyList()
+                );
     }
 }
